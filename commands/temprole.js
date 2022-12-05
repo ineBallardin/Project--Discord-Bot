113fa9f2b96f9d4fb5e75e58902fcace05589259
Karine <ineballardin@gmail.com>
2022-12-05 19:45:09 -0300

    refactoring the temprole command - by Arthur Azevedo

--- commands/temprole.js
+++ commands/temprole.js
@@ -5,14 +5,14 @@ module.exports = {
 		.setName('temprole')
 		.setDescription('Atribui cargo temporário para o usuário'),
 	async execute(interaction) {
-		const date = new Date;
-		const m = date.getMonth();
+		const currentMonth = new Date().getMonth();
 
-		const guildMemberManager = interaction.guild.members;
-		guildMemberManager.fetch().then(fetchedMembers => {
-			const totalMembers = fetchedMembers.filter(member => member.joinedAt.getMonth() >= m && member.joinedAt.getMonth() <= m);
-			console.log(totalMembers);
-		});
+		const { guild = {} } = interaction;
+		const guildMemberManager = guild.members;
+		const fetchedMembers = await guildMemberManager.fetch();
+		const filterMembers = member => member.joinedAt.getMonth() >= currentMonth && member.joinedAt.getMonth() <= currentMonth;
+		const totalMembers = fetchedMembers.filter(filterMembers);
+		console.log(totalMembers);
 
 		await interaction.reply({ content: 'ok', ephemeral: true });
 	},
