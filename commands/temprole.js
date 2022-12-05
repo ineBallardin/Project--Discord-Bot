c3a06a5caf4f1c385f28e6f7a23ca9592067348b
Karine <ineballardin@gmail.com>
2022-12-05 19:25:24 -0300

    fetching members by time they joined at the guild

--- commands/temprole.js
+++ commands/temprole.js
@@ -1,25 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-/* module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('usuario')
-		.setDescription('Retorna informações de um usuário.'),
-	async execute(interaction) {
-		await interaction.reply(`Comando executado por ${interaction.user.username}, que ingressou em ${interaction.member.joinedAt}.`);
-	},
-};*/
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('temprole')
 		.setDescription('Atribui cargo temporário para o usuário'),
 	async execute(interaction) {
 		const date = new Date;
-		const m = date.getMonth;
+		const m = date.getMonth();
 
 		const guildMemberManager = interaction.guild.members;
 		guildMemberManager.fetch().then(fetchedMembers => {
-			const totalMembers = fetchedMembers.filter(member => member.joinedAt.getMonth >= m && member.joinedAt <= m);
+			const totalMembers = fetchedMembers.filter(member => member.joinedAt.getMonth() >= m && member.joinedAt.getMonth() <= m);
 			console.log(totalMembers);
 		});
 
